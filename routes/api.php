31ea4cdfe300dc9d156ee13cfb2a8ad78e87891b
Geylian lingier <geylian.l@gmail.com>
2020-08-20 19:13:54 +0200

    create User model + create api route to login

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/user', function (Request $request) {
+    $data = $request->all();
+
+    $model = \App\User::create(
+        $data
+    );
+
+    return \App\User::find($model->id);
+});
