99af1d155cca534af3fb2e9bf9e53c8589bc2a70
Geylian lingier <geylian.l@gmail.com>
2020-08-24 18:08:57 +0200

    recreate User.php file + add has api token

--- app/User.php
+++ app/User.php
@@ -2,11 +2,39 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    protected $table = "users";
-    protected $primarykey = "id";
-    protected $fillable = ['firstname', 'lastname', 'username', 'email', 'password'];
+    use HasApiTokens, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstname', 'lastname', 'username', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
